df15902bfb2360572d8e62d6728c6ba03b059789
Gzim <gzimasanni@gmail.com>
2021-09-04 21:22:09 +0200

    Add validations

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,2 +1,7 @@
 class Flower < ApplicationRecord
+  has_one_attached :image
+
+  validates_presence_of :image
+  validates :title, presence: true, length: { minimum: 6, maximum: 80 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
