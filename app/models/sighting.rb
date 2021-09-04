69cb7f8e355f2cc9815daa3e022fa47f1d15dfa1
Gzim <gzimasanni@gmail.com>
2021-09-05 01:49:14 +0200

    Create likes controller

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,8 +3,9 @@ class Sighting < ApplicationRecord
 
   belongs_to :user
   belongs_to :flower
+   has_many :likes, foreign_key: 'sighting_id', class_name: 'Like', dependent: :destroy
 
-    # validates_presence_of :image
+  validates_presence_of :image
   validates :user_id, presence: true
   validates :flower_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
   has_many :sightings, foreign_key: 'user_id', class_name: 'Sighting'
+  has_many :likes, foreign_key: 'user_id', class_name: 'Like', dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 4, maximum: 24 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
